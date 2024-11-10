baddf09eafe3b0fa9bff21393c7964f7dcb677e0
Shubhansh Hardaha <kanhadas108@gmail.com>
2024-11-10 16:13:08 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -5,7 +7,6 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
-
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -14,7 +15,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = process.env.MONGODB_URI || 'mongodb+srv://dashboard:dashboard@cluster0.cuguj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
